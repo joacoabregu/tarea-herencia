293d4ee37395fd2baccdc4a8d14186be98c76c4f
Joaquin Abregu <abregujoaquin@gmail.com>
2021-06-01 22:21:01 -0300

    change subscription

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,7 @@ import User from './User';
 let usuario = new User(0, "joaco", "joaco@gmail.com", "credit card" );
 
 console.log(usuario);
+
+usuario.setSubscription = "basic";
+
+console.log(usuario)
